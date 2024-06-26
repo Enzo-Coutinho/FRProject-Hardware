a03c024d2ec0ca64433e42ade0e6ccab9c3f5cb9
Enzo-Coutinho <enzocoutinho71@gmail.com>
2024-06-26 07:51:55 -0300

    new: new class FocusLight

---
+++ lib/FocusLight/FocusLight.cpp
@@ -0,0 +1,27 @@
+#include "FocusLight.h"
+
+Freenove_ESP32_WS2812 FocusLight::FitaLED = Freenove_ESP32_WS2812(LED_COUNT, PINO, CHANNEL);
+
+FocusLight::FocusLight() {
+    FitaLED.begin();
+    FitaLED.setBrightness(40);
+}
+
+void FocusLight::calcule() {
+  while(Serial.available()) {
+    emotions[count] = Serial.read();
+    count += 1;
+  }
+
+  for(int i = 0; i <= 6; i++) {
+    emotionPercent[i] = emotions[i] / QUANTIDADE_PESSOAS;
+  }
+
+  percentTotal.emotionTotal[R] = (emotionPercent[ANGRY] + emotionPercent[SAD]);
+  percentTotal.emotionTotal[G] = (emotionPercent[HAPPY] + emotionPercent[NEUTRAL]);
+  percentTotal.emotionTotal[B] = (emotionPercent[DISGUST] + emotionPercent[FEAR] + emotionPercent[6]);
+}
+
+PercentTotal FocusLight::getPercentTotal() {
+    return percentTotal;
+}
---
+++ lib/FocusLight/FocusLight.h
@@ -0,0 +1,45 @@
+#ifndef FocusLight_h
+#define FocusLight_h
+#include "Freenove_WS2812_Lib_for_ESP32.h"
+#include "Arduino.h"
+
+struct PercentTotal {
+    int emotionTotal[3];
+};
+
+enum Emotions {
+    ANGRY = 0,
+    HAPPY = 1,
+    FEAR = 2,
+    SAD = 3,
+    DISGUST = 4,
+    NEUTRAL = 5,
+};
+
+enum Colors {
+    R = 0,
+    G = 1,
+    B = 2,
+};
+
+class FocusLight {
+    private:
+        PercentTotal percentTotal;
+        static Freenove_ESP32_WS2812 FitaLED;
+        static const int QUANTIDADE_PESSOAS = 10;
+        int count = 0;
+        int emotions[7] = {};
+        int emotionPercent[7] = {};
+        int emotionPercentTotal[3];
+        static const int CHANNEL = 0;
+        static const int PINO = 14;
+        static const int LED_COUNT = 30;
+    public:
+        void calcule();
+        PercentTotal getPercentTotal();
+        void setColor();
+        void rainbow();
+        FocusLight();
+};
+
+#endif
