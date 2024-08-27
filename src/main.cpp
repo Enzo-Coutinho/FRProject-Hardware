13a7e6c21af08d48d953209646b0203af6b72f35
Enzo-Coutinho <enzocoutinho71@gmail.com>
2024-08-27 18:14:09 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,12 @@
 #include "FocusLight.h"
 
 FocusLight controller;
-
 void setup() {
   Serial.begin(115200);
   controller.init();
 }
-
 void loop() {
   controller.readSerial();
   int* color = controller.calcule();
-  for(int i = 0; i<3; i++) {
-    Serial.print(color[i]);
-  }
-  Serial.println("");
   controller.setColor(color);
-  delay(100);
 }
